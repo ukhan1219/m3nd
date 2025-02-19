9f2b9ee2c14356d12846d89fae51686a7a8f3b8d
thashin04 <thashin2004@gmail.com>
2025-02-19 12:50:23 -0500

    fonts and component folder

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,8 +3,16 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    
   ],
   theme: {
+    colors: {
+      'pearl': '#F0EDF7',
+      'lavender':'#B6BDE7',
+      'lightblue': '#6F8BC5',
+      'midblue':'#3B4E6D',
+      'darkblue':'#434E16'
+    },
     extend: {},
   },
   plugins: [],
