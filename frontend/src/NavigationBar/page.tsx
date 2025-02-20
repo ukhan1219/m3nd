c44a420324979b266f230bdc836c2740d3f15914
Melanie <melanie.jinyoung@gmail.com>
2025-02-19 19:51:52 -0500

    added tailwind to navbar
    removed styles.css and implemented tailwind to the navbar design

--- frontend/src/NavigationBar/page.tsx
+++ frontend/src/NavigationBar/page.tsx
@@ -1,30 +1,38 @@
 import React from "react";
 import logo from "../assets/logo.png";
-import "./styles.css";
 
 const page = () => {
+    const navButtons = "text-[#262f50] text-sm px-4 py-2 rounded-lg hover:text-[#1a1a35]"
 
     return (
-        <nav className="navbar">
-            <div className="container">
-                {/* LOGO */}
-                <a href="#" className="logo">
-                    <img src={logo} alt="Logo" />
-                </a>
-            </div>
+        <nav className="fixed top-0 left-0 w-full bg-[#d5ddfa] p-5 flex items-center justify-between shadow-md z-50">
+            {/* LOGO */}
+            <a href="#" className="flex items-center">
+                <img src={logo} alt="Logo" className="h-7" />
+            </a>
 
             {/* LINKS */}
-            <div className="nav-buttons">
-                <button className="nav-btn">Home</button>
-                <button className="nav-btn">About</button>
-                <button className="nav-btn">Journals</button>
+            <div className="flex items-center gap-3">
+                <button className={navButtons}>
+                    Home
+                </button>
+                <button className={navButtons}>
+                    About
+                </button>
+                <button className={navButtons}>
+                    Journals
+                </button>
 
                 {/* AUTH BUTTONS */}
-                <button className="login-btn">Log in</button>
-                <button className="signup-btn">Sign up</button>
+                <button className="bg-[#262f50] text-[#d5ddfa] px-4 py-2 rounded-full text-sm hover:bg-[#2c2c92]">
+                    Log in
+                </button>
+                <button className="border-2 border-[#262f50] text-[#262f50] px-4 py-2 rounded-full text-sm hover:bg[#1a1a751">
+                    Sign up
+                </button>
             </div>
         </nav>
     )
 }
 
-export default page
+export default page;
