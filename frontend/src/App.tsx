1762a8dee85bc17f7d613fb9689919b562028c29
Melanie <melanie.jinyoung@gmail.com>
2025-02-19 20:17:46 -0500

    moved navigationbar into components
    also renamed the file and got rid of the folder since there is no more styles.css file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import NavigationBar from "./NavigationBar/page";
+import NavigationBar from "./components/NavigationBar";
 
 function App() {
   const [count, setCount] = useState(0)
