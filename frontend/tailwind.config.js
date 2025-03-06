fc86cc5625236538adfeaa89782b8e23e56f0c0e
thashin04 <thashin2004@gmail.com>
2025-03-06 18:41:38 -0500

    navbar finished

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,9 +3,27 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    
   ],
   theme: {
-    extend: {},
+    colors: {
+      'pearl': '#F0EDF7',
+      'lavender':'#B6BDE7',
+      'lightblue': '#6F8BC5',
+      'midblue':'#3B4E6D',
+      'darkblue':'#262F50'
+    },
+    fontFamily: {
+      Sora: ['Sora', 'serif'],
+      Kanit: ['Kanit', 'serif'],
+      Dongle: ['Dongle', 'serif'],
+    },
+    extend: {
+      screens: {
+        'min-2000': '2000px',
+    },
+
+    },
   },
   plugins: [],
 }
